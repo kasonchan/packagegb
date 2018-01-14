1386c6d8ef60d011c165579081dbfa443a1c6a17
kasonchan <kasonl.chan@gmail.com>
2018-01-14 15:38:51 +0000

    Update integration tests - Add sequential execution to test - Add checking of file downloaded and removed to isolate each test

--- pgb-it/src/test/scala/PgbIntegrationTestSuite.scala
+++ pgb-it/src/test/scala/PgbIntegrationTestSuite.scala
@@ -13,27 +13,37 @@ class PgbIntegrationTestSuite
   extends Specification
     with GWT
     with StandardDelimitedStepParsers {
-  def is: SpecStructure = s2"""
+
+  def is: SpecStructure = sequential ^ s2"""
 
  Given I enabled plugin PgbPlugin
-   When I enter sbt dlgb
-   Then I get the zip file and exit code {0} $dlgbSucceed
-
    When I type sbt dlgb invalidVersion
-   Then I get exit code status {0} but no zip file $dlgbFailed
+   Then I get exit code status {0} but no zip file {1} and removed {1} $dlgbFailed
 
+   When I enter sbt dlgb
+   Then I get exit code {0} and the zip file {0} and removed {0} $dlgbSucceed
   """
 
-  def dlgbSucceed: String => Fragments = example(anInt) { expectedExitCode =>
-    val exitCode: Int = Process("sbt dlgb").!
+  def dlgbFailed: String => Fragments = example(threeInts) { expectedExitCodes =>
+    val exitCode: Int = Process(Seq("sbt", "dlgb invalidVersion")).!
+    val fileExisted: Int = Process(Seq("find", "gatling-charts-highcharts-bundle-*")).!
 
-    exitCode must_== expectedExitCode
+    exitCode must_== expectedExitCodes._1
+    fileExisted must_== expectedExitCodes._2
+
+    val removedFile: Int = Process(Seq("/bin/sh", "-c", "rm gatling-charts-highcharts-bundle-*")).!
+    removedFile must_== expectedExitCodes._3
   }
 
-  def dlgbFailed: String => Fragments = example(anInt) { expectedExitCode =>
-    val exitCode: Int = Process(List("sbt", "dlgb invalidVersion")).!
+  def dlgbSucceed: String => Fragments = example(threeInts) { expectedExitCodes =>
+    val exitCode: Int = Process("sbt dlgb").!
+    val fileExisted: Int = Process(Seq("find", "gatling-charts-highcharts-bundle-*")).!
 
-    exitCode must_== expectedExitCode
+    exitCode must_== expectedExitCodes._1
+    fileExisted must_== expectedExitCodes._2
+
+    val removedFile: Int = Process(Seq("/bin/sh", "-c", "rm gatling-charts-highcharts-bundle-*")).!
+    removedFile must_== expectedExitCodes._3
   }
 
 }
