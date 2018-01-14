f8d3f08f59647700442a79ddd6c880914946f73c
kasonchan <kasonl.chan@gmail.com>
2018-01-14 23:19:25 +0000

    Fix test suite - Update unzip fail exit code to 9 - Add dlgb to unzipgbSucceed as preprocess and remove zip file as postprocess

--- pgb/src/test/scala/pgb/PgbTestSuite.scala
+++ pgb/src/test/scala/pgb/PgbTestSuite.scala
@@ -5,6 +5,7 @@ import org.specs2.specification.core.{Fragments, SpecStructure}
 import org.specs2.specification.script.StandardDelimitedStepParsers
 import pgb.Pgb.{dlgb, unzipgb}
 
+import scala.sys.process.Process
 import scala.util.{Failure, Success}
 
 /**
@@ -23,7 +24,7 @@ class PgbTestSuite
 
  When I call unzipgb(), I get a zip file and exit code {0} $unzipgbSucceed
 
- When I call unzipgb(invalidVersion), I get exit code status {1} $unzipgbFailed
+ When I call unzipgb(invalidVersion), I get exit code status {9} $unzipgbFailed
 
   """
 
@@ -46,12 +47,17 @@ class PgbTestSuite
   }
 
   def unzipgbSucceed: String => Fragments = example(anInt) { expectedExitCode =>
+    dlgb()
+
     val exitCode = unzipgb() match {
       case Success(s) => s
       case Failure(e) => e
     }
 
     exitCode must_== expectedExitCode
+
+    val removedFile: Int = Process(Seq("/bin/sh", "-c", "rm gatling-charts-highcharts-bundle-*")).!
+    removedFile must_== expectedExitCode
   }
 
   def unzipgbFailed: String => Fragments = example(anInt) { expectedExitCode =>
