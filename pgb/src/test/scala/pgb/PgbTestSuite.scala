b849008dc53604d95486eeb5be5a3a151fa25cb2
kasonchan <kasonl.chan@gmail.com>
2020-04-11 22:42:41 +0100

    Add test cleanups

--- pgb/src/test/scala/pgb/PgbTestSuite.scala
+++ pgb/src/test/scala/pgb/PgbTestSuite.scala
@@ -5,6 +5,7 @@ import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 import scala.util.{Success, Try}
+import scala.sys.process._
 
 /**
   * @author kasonchan
@@ -17,15 +18,22 @@ class PgbTestSuite extends AnyWordSpec with Matchers with TryValues {
   "pgb" must {
     "be able to download a current release version of gatling bundle when downloadGB() is called" in {
       Pgb.downloadGB() mustBe Try(expectedSuccessDownloadExitCode)
+      Process(Seq("/bin/sh",
+                  "-c",
+                  "rm -rv /tmp/gatling-charts-highcharts-bundle-*")).!
     }
 
     "be able to not to download a invalid version of gatling bundle when downloadGB() is called" in {
       Pgb.downloadGB("invalidVersion").success mustBe Success(
         expectedFailedDownloadExitCode)
+      Process(Seq("/bin/sh",
+                  "-c",
+                  "rm -rv /tmp/gatling-charts-highcharts-bundle-*")).!
     }
 
     "be able to deploy your project bundled with gatling when deployGB() is called" in {
       Pgb.deployGB().isSuccess mustBe true
+      Process(Seq("/bin/sh", "-c", "rm -rv pgb-scripts*")).!
     }
   }
 }
